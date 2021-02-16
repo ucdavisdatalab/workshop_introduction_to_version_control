05e88fde882e7341199326193669a2bce3e5f510
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2021-02-16 09:56:45 -0500

    added sections from IST-08X reader

---
+++ _common.R
@@ -0,0 +1,22 @@
+# Common code to run before knitting each chapter.
+
+url <- "./img/mac_finder_1.png"
+url2 <- "./img/mac_window_2.png"
+url3 <- "./img/spoke_and_where_cloud.png"
+url4 <- "./img/distributed_network.png"
+url5 <- "./img/hybrid_network.png"
+url6 <- "./img/empty_sectors.png"
+url7 <- "./img/filled_bits.png"
+url8 <- "./img/directory.png"
+url9 <- "./img/mac_finder_3.png"
+url10 <- "./img/version_tree.png"
+url11 <- "./img/TT_tree_mobile.jpg"
+url12 <- "./img/mac_terminal.png"
+url13 <- "./img/bash.png"
+url14 <- "./img/GIT-Branchand-its-Operations.png"
+url15 <- "./img/datalab-logo-full-color-rgb.png"
+url16 <- "./img/save_stage_commit.png"
+url17 <- "./img/git_for_teams.png"
+url18 <- "./img/branching_workflows.png"
+
+library(knitr)
